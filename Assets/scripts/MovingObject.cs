d8aa9a300a03f0d32f81b5fe5c9073f8c523f213
zhao Seo <tkddnjs5652@naver.com>
2020-02-23 13:40:00 +0900

    Walking animation

--- Assets/scripts/MovingObject.cs
+++ Assets/scripts/MovingObject.cs
@@ -18,42 +18,56 @@ public class MovingObject : MonoBehaviour
 
     private bool canMove = true;
 
+    private Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>(); //연결   
     }
     IEnumerator MoveCoroutine()
     {
-        if (Input.GetKey(KeyCode.LeftShift))
+        while(Input.GetAxisRaw("Vertical") != 0 || Input.GetAxisRaw("Horizontal") != 0)
         {
-            applyRunSpeed = runSpeed;
-            applyRunFlag = true
-        }
-        else
-        {
-            applyRunSpeed = 0;
-            applyRunFlag = false;
-        }
-        vector.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), transform.position.z);
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                applyRunSpeed = runSpeed;
+                applyRunFlag = true;
+            }
+            else
+            {
+                applyRunSpeed = 0;
+                applyRunFlag = false;
+            }
+            vector.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), transform.position.z);
 
-        while (currentWalkCount < walkCount)
-        {
             if (vector.x != 0)
+                vector.y = 0;
+
+            animator.SetFloat("DirX", vector.x);
+            animator.SetFloat("DirY", vector.y);
+            animator.SetBool("Walking", true);
+
+            while (currentWalkCount < walkCount)
             {
-                transform.Translate(vector.x * (speed + applyRunSpeed), 0, 0);
-            }
-            else if (vector.y != 0)
-            {
-                transform.Translate(0, vector.y * (speed + applyRunSpeed), 0);
-            }
-            if(applyRunFlag)
+                if (vector.x != 0)
+                {
+                    transform.Translate(vector.x * (speed + applyRunSpeed), 0, 0);
+                }
+                else if (vector.y != 0)
+                {
+                    transform.Translate(0, vector.y * (speed + applyRunSpeed), 0);
+                }
+                if (applyRunFlag)
+                    currentWalkCount++;
                 currentWalkCount++;
-            currentWalkCount++;
-            yield return new WaitForSeconds(0.01f);
+                yield return new WaitForSeconds(0.01f);
+            }
+            currentWalkCount = 0;    
         }
-        currentWalkCount = 0;
+        animator.SetBool("Walking", false);
         canMove = true;
+
     }
     // Update is called once per frame
     void Update()
