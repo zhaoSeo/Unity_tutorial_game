1dd31107ba33ec00726d392639e6b648794c67b7
zhao Seo <tkddnjs5652@naver.com>
2020-02-27 21:47:42 +0900

    create camera bound

--- Assets/scripts/CameraManager.cs
+++ Assets/scripts/CameraManager.cs
@@ -10,17 +10,40 @@ public class CameraManager : MonoBehaviour
     public float moveSpeed; //카메라가 얼마나 빠른 속도로 따라가는지
     private Vector3 targetPosition; //대상의 현재 위치 값
 
-    // Start is called before the first frame update
-    void Start()
+    public BoxCollider2D bound;
+    private Vector3 minBound;
+    private Vector3 maxBound;
+    //박스컬라이더 영역의 최소 최대 xyz값을 지님.
+
+    private float halfWidth;
+    private float halfHeight;
+    //카메라의 반너비, 반높 값을 지닐 변수
+
+    private Camera theCamera;
+    //카메라의 반높이값을 구할 속성을 이용하기 위한 변수
+
+    private void Awake()
     {
-        if(instance != null)
+        if (instance != null)
         {
             Destroy(this.gameObject);
-        } else
+        }
+        else
         {
             DontDestroyOnLoad(this.gameObject);
             instance = this;
         }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theCamera = GetComponent<Camera>();
+        minBound = bound.bounds.min;
+        maxBound = bound.bounds.max;
+        halfHeight = theCamera.orthographicSize;
+        halfWidth = halfHeight * Screen.width / Screen.height;
+
         
     }
 
@@ -32,6 +55,11 @@ public class CameraManager : MonoBehaviour
             targetPosition.Set(target.transform.position.x, target.transform.position.y, this.transform.position.z);
             this.transform.position = Vector3.Lerp(this.transform.position, targetPosition, moveSpeed * Time.deltaTime); //1초에 movespeed만큼 이동
             //lerp 중간값 반환
+
+            float clampedX = Mathf.Clamp(this.transform.position.x, minBound.x + halfWidth, maxBound.x - halfWidth);
+            float clampedY = Mathf.Clamp(this.transform.position.y, minBound.y + halfHeight, maxBound.y - halfHeight);
+
+            this.transform.position = new Vector3(clampedX, clampedY, this.transform.position.z);
         }
     }
 }
