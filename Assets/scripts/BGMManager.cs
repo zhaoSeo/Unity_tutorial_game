63c461d3fb5fd8d6863c40f883f1d5663ee33b83
zhao Seo <tkddnjs5652@naver.com>
2020-02-29 16:40:34 +0900

    create bgm manager

---
+++ Assets/scripts/BGMManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMManager : MonoBehaviour
+{
+    static public BGMManager instance;
+    public AudioClip[] clips;
+
+    private AudioSource source;
+
+    private WaitForSeconds waitTime = new WaitForSeconds(0.01f);
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(this.gameObject);
+            instance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+    public void SetVolume(float _vloume)
+    {
+        source.volume = _vloume;
+    }
+    public void Play(int _playMusicTrack)
+    {
+        source.volume = 1f;
+        source.clip = clips[_playMusicTrack];
+        source.Play();
+    }
+    public void Pause()
+    {
+        ) {
+            source.Pause();
+        }
+    }
+    public void Unpause()
+    {
+        source.UnPause();
+    }
+    public void Stop()
+    {
+        source.Stop();
+    }
+
+    IEnumerator FadeOutMusicCoroutine()
+    {
+        for(float i = 1.0f; i >= 0f; i-= 0.01f)
+        {
+            source.volume = i;
+            yield return waitTime;
+        }
+    }
+    IEnumerator FadeInMusicCoroutine()
+    {
+        for (float i = 0f; i <= 1f; i+= 0.01f)
+        {
+            source.volume = i;
+            yield return waitTime;
+        }
+    }
+    public void FadeOutMusic()
+    {
+        StopAllCoroutines();
+        StartCoroutine(FadeOutMusicCoroutine());
+    }
+    public void FadeInMusic()
+    {
+        StopAllCoroutines();
+        StartCoroutine(FadeInMusicCoroutine());
+    }
+
+}
---
+++ Assets/scripts/TestScript1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript1 : MonoBehaviour
+{
+
+    BGMManager BGM;
+    public int playMusicTrack;
+    // Start is called before the first frame update
+    void Start()
+    {
+        BGM = FindObjectOfType<BGMManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        BGM.Play(playMusicTrack);
+        this.gameObject.SetActive(false);
+    }
+}
