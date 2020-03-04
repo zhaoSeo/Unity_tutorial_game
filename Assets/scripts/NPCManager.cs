d8be35d6bcb9a41c2e32647d3344159eb23b3840
zhao Seo <tkddnjs5652@naver.com>
2020-03-04 13:40:04 +0900

    create dialogue system 1

--- Assets/scripts/NPCManager.cs
+++ Assets/scripts/NPCManager.cs
@@ -36,23 +36,6 @@ public class NPCManager : MovingObject
         {
             for(int i = 0; i < npc.direction.Length; i++)
             {
-                switch(npc.frequency)
-                {
-                    case 1:
-                        yield return new WaitForSeconds(4f);
-                        break;
-                    case 2:
-                        yield return new WaitForSeconds(3f);
-                        break;
-                    case 3:
-                        yield return new WaitForSeconds(2f);
-                        break;
-                    case 4:
-                        yield return new WaitForSeconds(1f);
-                        break;
-                    case 5:
-                        break;
-                }
                 yield return new WaitUntil(() => npcCanMove);
                 base.Move(npc.direction[i], npc.frequency);
 
